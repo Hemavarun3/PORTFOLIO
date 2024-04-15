0faaf8f8e98b7a942254bdcff5d9c124d99d4dd8
Hemavarun3 <21uec064@lnmiit.ac.in>
2024-04-16 03:01:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'; // Import Routes and Navigate from react-router-dom for React 18 compatibility
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'; 
 import Intro from './components/Intro/intro';
 import About from './components/About/about';
 import Header from './components/Header/Navbar';
 import Projects from './components/projects/projects';
 import Footer from './components/Footer/footer';
 
+
 function App() {
   return (
-    <Router>
-      <div>
-        <Header />
-        <Routes>
-          <Route path="/home" element={<Intro />} />
-          <Route path="/" element={<Intro />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
-        <Footer />
-      </div>
+    <Router basename="/">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Intro />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+      <Footer />
     </Router>
   );
 }
