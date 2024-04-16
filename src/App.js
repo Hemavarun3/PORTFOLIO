025919cd0fb907af2c0dcb8f3dc993fc44464ff6
Hemavarun3 <21uec064@lnmiit.ac.in>
2024-04-16 22:48:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { Outlet} from 'react-router-dom'; // Import Routes and Navigate from react-router-dom for React 18 compatibility
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import BrowserRouter, Routes, and Route
 import Header from './components/Header/Navbar';
 import Footer from './components/Footer/footer';
-
+import About from './components/About/about';
+import Intro from './components/Intro/intro';
+import Project from './components/projects/projects';
 
 function App() {
   return (
+    <Router>
       <div>
         <Header />
-        <Outlet />
+        <Routes>
+          <Route path="/" element={<Intro />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Project />} />
+        </Routes>
         <Footer />
       </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,35 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom'; // Importing createRoot from react-dom
 import App from './App';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import About from './components/About/about';
-import Intro from './components/Intro/intro';
-import Project from './components/projects/projects';
+
 
 // Create your routes using createBrowserRouter
-const routes = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: 'about',
-        element: <About />
-      },
-      {
-        path: 'home',
-        element: <Intro />
-      },
-      {
-        path: 'projects',
-        element: <Project />
-      }
-    ]
-  }
-]);
+
 
 // Use createRoot to render your application
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <RouterProvider router={routes} />
+  <App />
 );
