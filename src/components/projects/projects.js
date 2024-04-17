3cccac06ff9998ec2e04d764c3f11d3b6cc4494d
Hemavarun3 <21uec064@lnmiit.ac.in>
2024-04-17 15:09:15 +0530

    Project card component update

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './project.css';
 import Pcard from './pcard';
@@ -26,14 +27,14 @@ const data = [
   },
   {
     "name": "AMUL-WEBSITE",
-    "key": "2",
+    "key": "4",
     "img": "../../assets/food_delivery.jpg",
     "link": "https://weather-app-l53f.onrender.com/",
     "desc": "This is an E-commerce application built using MERN stack.Tech Stack Used: ExpressJS, NodeJS, ReactJs, MongoDB, Javascript,Html,Css."
   },
   {
     "name": "AMUL-WEBSITE",
-    "key": "3",
+    "key": "5",
     "img": "../../assets/food_delivery.jpg",
     "link": "https://weather-app-l53f.onrender.com/",
     "desc": "This is an E-commerce application built using MERN stack.Tech Stack Used: ExpressJS, NodeJS, ReactJs, MongoDB, Javascript,Html,Css."
@@ -42,10 +43,10 @@ const data = [
 
 export default function Project() {
   return (
-    <div className="container">
-      <div className="row"> 
-        {data.map((item) => (
-          <div className="col-md-4 card" key={item.key}>
+    <div className="container"> 
+      <div className="row">
+        {data.map(item => (
+          <div className="col-4 card" key={item.key}>
             <Pcard params={item} />
           </div>
         ))}
