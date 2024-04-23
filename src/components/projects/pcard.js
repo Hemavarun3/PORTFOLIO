ce900f9de0eaa47b11165ce48a342a8b6a848db7
Hemavarun3 <21uec064@lnmiit.ac.in>
2024-04-23 20:10:13 +0530

    final changes

--- src/components/projects/pcard.js
+++ src/components/projects/pcard.js
@@ -4,7 +4,7 @@ import './pcard.css';
 function PCard(props) {
   return (
     <div className="card shadow  rounded">
-      <a href={props.params.link} title={props.params.name}><img src={props.params.img} alt={props.params.name} /></a>
+      <a href={props.params.link} title={props.params.name}><img src={(props.params.img)} alt={props.params.name} /></a>
       <h2>{props.params.name}</h2>
       <p>{props.params.desc}</p>
       <span><a href={props.params.github}><button className='btn'>GITHUB</button></a><a href={props.params.demo}><button className='btn'>DEMO</button></a></span>
--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './project.css';
-import Pcard from './pcard';
+import cimg1 from "./images/food_delivery.jpg"
+import cimg2 from "./images/weatherapp.png"
+import cimg3 from "./images/portfolio.png"
 
 const data = [
   {
     "name": "AMUL-WEBSITE",
     "key": "1",
-    "img": "src/assets/food_delivery.jpg",
+    "img": "./images/food_delivery.jpg",
     "github": "https://github.com/Hemavarun3/AMUL-WEBSITE",
     "demo": "https://github.com/Hemavarun3/AMUL-WEBSITE",
     "desc": "This is an E-commerce application built using MERN stack.Tech Stack Used: ExpressJS, NodeJS, ReactJs, MongoDB, Javascript,Html,Css."
@@ -15,7 +17,7 @@ const data = [
   {
     "name": "WEATHER-APP",
     "key": "2",
-    "img": "src/assets/weatherapp.png",
+    "img": "./assets/weatherapp.png",
     "github": "https://github.com/Hemavarun3/Weather-App",
     "demo": "https://weather-app-l53f.onrender.com/",
     "desc": "Designed a Weather App using HTML, CSS, and JavaScript, enabling real-time weather updates and location-based functionalities."
@@ -23,7 +25,7 @@ const data = [
   {
     "name": "PORTFOLIO",
     "key": "3",
-    "img": "src/assets/portfolio.png",
+    "img": "./assets/portfolio.png",
     "github": "https://github.com/Hemavarun3/PORTFOLIO",
     "demo": "https://hemavarun-portfolio.netlify.app/",
     "desc": "Designed a Portfolio website using ReactJs, HTML, CSS, and JavaScript. using various prebuilt features in reactJs like Timeline Components etc."
@@ -34,11 +36,24 @@ export default function Project() {
   return (
     <div className="container"> 
       <div className="row">
-        {data.map(item => (
-          <div className="col-4 card" key={item.key}>
-            <Pcard params={item} />
-          </div>
-        ))}
+      <div className="col-4 card rounded">
+        <a href={data[0].demo} title={data[0].name}><img src={cimg1} alt={data[0].name} /></a>
+          <h2>{data[0].name}</h2>
+          <p>{data[0].desc}</p>
+          <span><a href={data[0].github}><button className='btn'>GITHUB</button></a><a href={data[0].demo}><button className='btn'>DEMO</button></a></span>
+      </div>
+      <div className="card rounded col-4">
+        <a href={data[1].demo} title={data[1].name}><img src={cimg2} alt={data[1].name} /></a>
+          <h2>{data[1].name}</h2>
+          <p>{data[1].desc}</p>
+          <span><a href={data[1].github}><button className='btn'>GITHUB</button></a><a href={data[1].demo}><button className='btn'>DEMO</button></a></span>
+      </div>
+      <div className="card rounded col-4">
+        <a href={data[2].demo} title={data[2].name}><img src={cimg3} alt={data[2].name} /></a>
+          <h2>{data[2].name}</h2>
+          <p>{data[2].desc}</p>
+          <span><a href={data[2].github}><button className='btn'>GITHUB</button></a><a href={data[2].demo}><button className='btn'>DEMO</button></a></span>
+      </div>
       </div>
     </div>
   );
